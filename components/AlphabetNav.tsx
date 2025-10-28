4062dbc2c4646ae2bb57e1192bfab17b07a847ad
saiminh <saiminh@gmail.com>
2025-10-28 16:06:05 +0100

    add mobile style

--- components/AlphabetNav.tsx
+++ components/AlphabetNav.tsx
@@ -8,19 +8,22 @@ interface AlphabetNavProps {
 
 export default function AlphabetNav({ entries }: AlphabetNavProps) {
   return (
-    <ul className="flex gap-4">
-      {Object.keys(entries).sort((a, b) => {
-          // Special sorting for group keys
-          if (a === '0-9') return 1; // Numbers first
-          if (b === '0-9') return -1;
-          if (a === 'X-Z') return 1; // X-Z last
-          if (b === 'X-Z') return -1;
-          return a.localeCompare(b); // Regular alphabetical for letters
-        }).map(letter => (
-        <li key={letter} className="text-xl text-qreen-dark font-[700]">
-          <Link href={`#${letter}`}>{letter}</Link>
-        </li>
-      ))}
-    </ul>
+    <div className="relative">
+      <ul className="flex gap-4 w-full overflow-x-auto no-scrollbar pr-20 md:pr-0">
+        {Object.keys(entries).sort((a, b) => {
+            // Special sorting for group keys
+            if (a === '0-9') return 1; // Numbers first
+            if (b === '0-9') return -1;
+            if (a === 'X-Z') return 1; // X-Z last
+            if (b === 'X-Z') return -1;
+            return a.localeCompare(b); // Regular alphabetical for letters
+          }).map(letter => (
+          <li key={letter} className="text-xl text-qreen-dark font-[700]">
+            <Link href={`#${letter}`} className="whitespace-nowrap">{letter}</Link>
+          </li>
+        ))}
+      </ul>
+      <div className="absolute right-0 top-0 h-full w-20 bg-gradient-to-l from-qaupe to-transparent z-10 pointer-events-none"></div>
+    </div>
   )
 }
