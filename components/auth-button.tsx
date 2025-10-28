8949af487fcc56cdb799dd6ed85e7185b6b153de
saiminh <saiminh@gmail.com>
2025-10-28 10:01:08 +0100

    login form

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -3,9 +3,10 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { createClient } from "@/lib/supabase/client";
-import { LogoutButton } from "./logout-button";
+import { LogoutButton, LogoutMenuItem } from "./logout-button";
 import { useEffect, useState } from "react";
 import type { User } from "@supabase/supabase-js";
+import { UserRound } from "lucide-react";
 
 export function AuthButton() {
   const [user, setUser] = useState<User | null>(null);
@@ -45,33 +46,38 @@ export function AuthButton() {
   }
 
   return user ? (
-    <div className="flex items-center gap-4">
-      <Link href="/protected" className="text-sm">
-        Hey, {user.email}!
-      </Link>
-      <LogoutButton />
+    <div className="group relative flex-col items-center gap-4">
+      <span className="inline-block p-1 border-2 border-qreen text-qreen rounded-full">
+        <UserRound className="w-6 h-6" /> 
+      </span>
+      <div className="absolute top-full right-0 w-full min-w-56 hidden group-hover:block bg-qaupe p-4 rounded-lg space-y-2 border-2 border-qreen">
+        <p className="">
+          Hello, {user.email}!
+        </p>
+        <Link href="/protected" className="flex-col font-semibold text-qreen items-center gap-1 block">
+          Membership Settings
+        </Link>
+        <LogoutMenuItem />
+      </div>
     </div>
   ) : (
     <div className="flex gap-8 justify-between w-full">
       <Link href="/auth/login" className="font-medium flex gap-2 items-center">
         <span>
-        <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.2201 6.6105V3.9705C13.2201 2.3205 11.8701 0.970497 10.2201 0.970497H5.85008C4.20008 0.970497 2.85008 2.3205 2.85008 3.9705V6.6105H0.0800781V15.8905C0.0800781 18.1005 1.87008 19.8905 4.08008 19.8905H12.0001C14.2101 19.8905 16.0001 18.1005 16.0001 15.8905V6.6105H13.2201ZM4.86008 3.9705C4.86008 3.4205 5.31008 2.9705 5.86008 2.9705H10.2301C10.7801 2.9705 11.2301 3.4205 11.2301 3.9705V6.6105H4.86008V3.9705ZM14.0001 15.8905C14.0001 16.9905 13.1001 17.8905 12.0001 17.8905H4.08008C2.98008 17.8905 2.08008 16.9905 2.08008 15.8905V8.6105H14.0001V15.8905Z" fill="currentColor"/>
-          <path d="M9.42008 15.9605L8.69008 12.7705C9.12008 12.5405 9.42008 12.0905 9.42008 11.5605C9.42008 10.8005 8.80008 10.1805 8.04008 10.1805C7.28008 10.1805 6.66008 10.8005 6.66008 11.5605C6.66008 12.0805 6.96008 12.5305 7.39008 12.7705L6.66008 15.9605H9.42008Z" fill="currentColor"/>
+          <svg width="16" height="20" viewBox="0 0 16 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.2201 6.6105V3.9705C13.2201 2.3205 11.8701 0.970497 10.2201 0.970497H5.85008C4.20008 0.970497 2.85008 2.3205 2.85008 3.9705V6.6105H0.0800781V15.8905C0.0800781 18.1005 1.87008 19.8905 4.08008 19.8905H12.0001C14.2101 19.8905 16.0001 18.1005 16.0001 15.8905V6.6105H13.2201ZM4.86008 3.9705C4.86008 3.4205 5.31008 2.9705 5.86008 2.9705H10.2301C10.7801 2.9705 11.2301 3.4205 11.2301 3.9705V6.6105H4.86008V3.9705ZM14.0001 15.8905C14.0001 16.9905 13.1001 17.8905 12.0001 17.8905H4.08008C2.98008 17.8905 2.08008 16.9905 2.08008 15.8905V8.6105H14.0001V15.8905Z" fill="currentColor"/>
+            <path d="M9.42008 15.9605L8.69008 12.7705C9.12008 12.5405 9.42008 12.0905 9.42008 11.5605C9.42008 10.8005 8.80008 10.1805 8.04008 10.1805C7.28008 10.1805 6.66008 10.8005 6.66008 11.5605C6.66008 12.0805 6.96008 12.5305 7.39008 12.7705L6.66008 15.9605H9.42008Z" fill="currentColor"/>
         </svg>
-
         </span>
         Log in
       </Link>
       {/* <Button asChild size="sm" variant={"default"}>
         <Link href="/auth/sign-up">Sign up</Link>
       </Button> */}
-      <Button variant={"qreen"}>
+      <Button variant={"qreen"} className="[&_svg]:size-5">
         <Link href='/members/new-membership-application' className="flex items-center gap-1">
           <span>
-          <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M1 17.4305C1 13.6007 4.13233 10.4991 8 10.4991C11.8677 10.4991 15 13.6007 15 17.4305M12.5839 5.9695C12.5839 8.47633 10.5316 10.5085 8 10.5085C5.46838 10.5085 3.4161 8.47633 3.4161 5.9695C3.4161 3.46268 5.46838 1.4305 8 1.4305C10.5316 1.4305 12.5839 3.46268 12.5839 5.9695Z" stroke="currentColor" strokeWidth="2" strokeMiterlimit="10"/>
-          </svg>
+            <UserRound className="w-5 h-5" />
           </span>
           Join AQR
         </Link>
