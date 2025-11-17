c0e6e2df4da00b9730272401a49ef50bcffb3442
saiminh <saiminh@gmail.com>
2025-11-17 13:45:34 +0100

    forgot password layout change

--- components/forgot-password-form.tsx
+++ components/forgot-password-form.tsx
@@ -45,7 +45,7 @@ export function ForgotPasswordForm({
   };
 
   return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
+    <div className={cn("form flex flex-col gap-6", className)} {...props}>
       {success ? (
         <Card>
           <CardHeader>
