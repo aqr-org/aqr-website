03d92ac436dfd49a08bed280e169de12f0023ff7
saiminh <saiminh@gmail.com>
2025-11-17 10:44:23 +0100

    update logo

--- components/navigation/NavigationLogo.tsx
+++ components/navigation/NavigationLogo.tsx
@@ -3,7 +3,7 @@ import Logo from "@/components/Logo";
 
 export default function NavigationLogo() {
   return (
-    <Link href="/" className="w-20 h-20 inline-flex items-center" aria-label="Home">
+    <Link href="/" className="w-20 h-20 md:w-[88px] md:h-[77px] inline-flex items-center" aria-label="Home">
       <Logo />
     </Link>
   );
